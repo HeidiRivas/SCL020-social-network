a002b4ca4e57e0eed18f74924cbd6266330eb528
Javiera <jbkc_10@hotmail.com>
2022-06-14 20:24:28 -0400

    cambio lugar credenciales

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import  "https://www.gstatic.com/firebasejs/9.8.3/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCLS-C_h-KKvDJTUOI8fH_KWI9K--MnHBA",
+    authDomain: "fancosfic.firebaseapp.com",
+    projectId: "fancosfic",
+    storageBucket: "fancosfic.appspot.com",
+    messagingSenderId: "597841723430",
+    appId: "1:597841723430:web:d4e54da74528088a8466e7"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
