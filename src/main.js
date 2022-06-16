53b4b00f554192101812932a4978bcc6ef16bde0
Sol <solemich@gmail.com>
2022-06-16 00:18:00 -0400

    config

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import {initRouter} from "./router/router.js"
 import {login} from "./firebase/auth.js"
 import Welcome from "./views/welcome.js"
 import firebaseConfig from "./firebase/config.js"
+
   initRouter()
 
   
