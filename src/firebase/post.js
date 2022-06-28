4e060ccc79bf94cff5a791df55c9d984fc3e0fb7
HeidiRivas <102159190+HeidiRivas@users.noreply.github.com>
2022-06-28 19:38:39 -0400

    botonesDeEdityDelete

--- src/firebase/post.js
+++ src/firebase/post.js
@@ -1,5 +1,5 @@
 
-import {db, collection, getDocs,addDoc} from './init.js'
+import {db, collection, getDocs,addDoc,onSnapshot} from './init.js'
 
 
 const savePost = async (data)=> {
@@ -11,17 +11,23 @@ const savePost = async (data)=> {
       }
 }
 
+
+ 
+
 const listPost = async ()=> {
   const querySnapShot = await getDocs(collection(db, "post"));
-  
+  console.log(querySnapShot)
   const taskContainer = document.getElementById("task-container");
   
    
  let html = ''
   querySnapShot.forEach(doc => {
      html += `
-     <div>
+     <div class= 'postOld'>
      <h3> ${doc.data().content}</h3>
+     <button>Editar</button>
+     <button>Borrar</button>
+    <a href='../img/conLike.png'><img src='../img/sinLike.png' class="sinLike" ></a>
      </div>
     `
   });
